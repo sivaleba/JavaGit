4f4a25a2850adbea55a8750d255a0f5c1bcff1bc
jay_l <jay_l@LAPTOP-57721RJI>
2022-06-08 22:53:35 -0500

    MasterTest

---
+++ src/classwork/AnimalWorld.java
@@ -0,0 +1,12 @@
+package classwork;
+
+public class AnimalWorld {
+
+	public static void main(String[] args) {
+		System.out.println("This is animal world");
+		System.out.println("Animal world: dog");
+		System.out.println("Animal world : Peacock");
+
+	}
+
+}
